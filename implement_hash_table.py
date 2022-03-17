2214f0cee83b4fd9cf9a697362d7a138f3c3cd87
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-03-16 21:42:18 -0500

    Update implement_hash_table.py

--- implement_hash_table.py
+++ implement_hash_table.py
@@ -1,6 +1,8 @@
 class HashTable(object):
     def __init__(self):
-        self.capacity = 50
-        self.size = 0
-        self.buckets = [None] * self.capacity
+        self.array = [None] * 50
+
+    def hash(self, key):
+        length = len(self.array)
+        return hash(key) % length
 
