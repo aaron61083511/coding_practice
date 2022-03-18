3b76467354262bb901bb37e5e0284086f0e0a14c
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-03-18 08:54:48 -0500

    Create implement_linked_list.py

---
+++ implement_linked_list.py
@@ -0,0 +1,55 @@
+class Node(object):
+    def __init__(self, node = None):
+        self.node = node
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        printvalue = self.head
+        while printvalue is not None:
+            print(printvalue.node)
+            printvalue = printvalue.next
+
+    def get_head(self):
+        if self.head:
+            return self.head.node
+        else:
+            print('Head is Null')
+
+    def get_length(self):
+        count = 0
+        val = self.head
+        while val:
+            count += 1
+            val = val.next
+        return count
+
+    def add_head(self, newnode):
+        new = Node(newnode)
+        new.next = self.head
+        self.head = new
+
+    # def find_end(self):
+
+
+    def add_end(self, newnode):
+        new = Node(newnode)
+
+
+
+
+list = LinkedList()
+list.head = Node('A')
+value2 = Node('B')
+value3 = Node('D')
+list.head.next = value2
+value2.next = value3
+list.add_head('start')
+list.add_head('')
+list.add_head('sd')
+list.print_list()
+print(list.get_head())
+print(list.get_length())
