e1f73f52cef12de71dd0b31944c89ffceefe4622
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-03-24 21:11:35 -0500

    Update implement_hash_table.py

--- implement_hash_table.py
+++ implement_hash_table.py
@@ -3,12 +3,25 @@ class Node(object):
         self.key = key
         self.node = node
         self.next = None
-
+        self.prev = None
 
 class HashTable(object):
     def __init__(self):
         self.array = [None] * 50
+        self.head = None
+        self.tail = None
 
     def hash(self, key):
         length = len(self.array)
         return hash(key) % length
+
+    def add(self, key, value):
+        index = self.hash(key)
+        node = self.array[index]
+        new = Node(key, value)
+        if node is None:
+            self.array[index] = new
+
+
+
+
