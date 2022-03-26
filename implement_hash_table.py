2c408a65ddf62bb35914b932b5d168fc264fdb28
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-03-25 20:41:13 -0500

    Update implement_hash_table.py

--- implement_hash_table.py
+++ implement_hash_table.py
@@ -1,27 +1,76 @@
 class Node(object):
-    def __init__(self, key, node):
+    def __init__(self, key, value):
         self.key = key
-        self.node = node
+        self.value = value
         self.next = None
         self.prev = None
 
+
 class HashTable(object):
     def __init__(self):
-        self.array = [None] * 50
+        self.array = [None] * 10
         self.head = None
         self.tail = None
 
     def hash(self, key):
         length = len(self.array)
-        return hash(key) % length
+        return key % length
 
-    def add(self, key, value):
-        index = self.hash(key)
-        node = self.array[index]
+    def print_list_item(self, key):
+        if self.array[hash(key)] is not None:
+            print_value = self.head
+            while print_value is not None:
+                print(print_value.value)
+                print_value = print_value.next
+        else:
+            print('Value under this key is None')
+
+# insert here is the default insert: add value at the tail of chain if collision
+    def insert(self, key, value):
         new = Node(key, value)
-        if node is None:
-            self.array[index] = new
+        if self.array[hash(key)] is None:
+            self.array[hash(key)] = new
+            self.head = new
+            self.tail = new
+        else:
+            self.tail.next = new
+            new.prev = self.tail
+            self.tail = new
+
+    def remove(self, key, value):
+        current = self.array[hash(key)]
+        if self.array[hash(key)] is None:
+            print('key value pair does not exist')
+        else:
+            while current is not None:
+                if current.value == value:
+                    break
+                current = current.next
+            if current is None:
+                print('key value pair does not exist')
+            elif current == self.head:
+                if current.next is None:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.next.prev = None
+                    self.head = current.next
+            elif current == self.tail:
+                if current.prev is None:
+                    self.tail = None
+                    self.head = None
+                else:
+                    current.prev.next = None
+                    self.tail = current.prev
+            else:
+                current.next.prev = current.prev
+                current.prev.next = current.next
 
 
 
-
+hashmap = HashTable()
+# hashmap.insert(1, 'A')
+hashmap.insert(1, 'B')
+# hashmap.insert(1, 'C')
+hashmap.remove(1, 'B')
+hashmap.print_list_item(1)
