fc56439620da12beabb0ab55f678afc9d27e160b
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-03-28 10:46:48 -0500

    Update implement_priority_queue.py

--- implement_priority_queue.py
+++ implement_priority_queue.py
@@ -163,7 +163,6 @@ class Pqueue(object):
             current = self.tail
             counter = -1
             while current is not None:
-                print(current.priority, current.value, counter)
                 if counter == index:
                     break
                 current = current.prev
@@ -173,12 +172,126 @@ class Pqueue(object):
             else:
                 print(current.priority, current.value)
 
+    def remove_item_from_index(self, index):
+        if self.head is None:
+            print('Empty PQ')
+        elif index >= 0:
+            current = self.head
+            counter = 0
+            while current is not None:
+                if counter == index:
+                    break
+                current = current.next
+                counter += 1
+            if current is None:
+                print('Index out of range')
+            elif current == self.head:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.next.prev = None
+                    self.head = current.next
+            elif current == self.tail:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.prev.next = None
+                    self.tail = current.prev
+            else:
+                current.next.prev = current.prev
+                current.prev.next = current.next
+        else:
+            current = self.tail
+            counter = -1
+            while current is not None:
+                if counter == index:
+                    break
+                current = current.prev
+                counter -= 1
+            if current is None:
+                print('Index out of range')
+            elif current == self.head:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.next.prev = None
+                    self.head = current.next
+            elif current == self.tail:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.prev.next = None
+                    self.tail = current.prev
+            else:
+                current.next.prev = current.prev
+                current.prev.next = current.next
+        self.size -= 1
 
-    # def remove_item_from_index(self, index):
-    #
-    # def remove_item_from_value(self, value):
-    #
-    # def remove_item_from_priority(self, priority):
+    def remove_item_from_value(self, value):
+        # remove first value appears
+        if self.head is None:
+            print('Empty PQ')
+        else:
+            current = self.head
+            while current is not None:
+                if current.value == value:
+                    break
+                current = current.next
+            if current is None:
+                print('Value not in PQ')
+            elif current == self.head:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.next.prev = None
+                    self.head = current.next
+            elif current == self.tail:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.prev.next = None
+                    self.tail = current.prev
+            else:
+                current.next.prev = current.prev
+                current.prev.next = current.next
+        self.size -= 1
+
+    def remove_item_from_priority(self, priority):
+        # remove first from priority level
+        if self.head is None:
+            print('Empty PQ')
+        else:
+            current = self.head
+            while current is not None:
+                if current.priority == priority:
+                    break
+                current = current.next
+            if current is None:
+                print('Priority not in PQ')
+            elif current == self.head:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.next.prev = None
+                    self.head = current.next
+            elif current == self.tail:
+                if self.size == 1:
+                    self.head = None
+                    self.tail = None
+                else:
+                    current.prev.next = None
+                    self.tail = current.prev
+            else:
+                current.next.prev = current.prev
+                current.prev.next = current.next
+        self.size -= 1
 
 
 
@@ -196,5 +309,9 @@ pq.insert_with_priority('AA2', 2)
 # pq.get_index_from_priority(2)
 # pq.get_priority_from_value('AAA')
 # pq.get_value_from_priority(5)
-pq.get_value_priority_from_index(-4)
+# pq.get_value_priority_from_index(-8)
+# pq.remove_item_from_index(-3)
+# pq.remove_item_from_value('')
+pq.remove_item_from_priority(6)
 pq.print_pq()
+
