548292784a85832473578f33c3ad89870551de94
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-04-19 16:59:25 -0500

    Update 39_combination_sum.py

---
+++ combo_based_dfs/39_combination_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        path_sum = 0
+        def backtracking(candidates, path=[], start=0):
+            path_sum = sum(path)
+            if path_sum == target:
+                result.append(path[::])
+            if path_sum > target:
+                return
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                path_sum += candidates[i]
+                backtracking(candidates, path, i)
+                path.pop()
+                path_sum -= candidates[i]
+
+        backtracking(candidates)
+        return result
