6445e7f01632590d91739e11ce7fe2da4a919d9a
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-04-25 12:26:05 -0500

    Update 704_binary_search.py

--- binary_search/704_binary_search.py
+++ binary_search/704_binary_search.py
@@ -1,16 +1,13 @@
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         left = 0
-        right = len(nums) - 1
-        if nums[left] <= nums[right] or len(nums) == 0:
-            return nums[left]
+        right = len(nums)-1
         while left <= right:
             mid = left + (right-left)//2
-            if nums[mid] > nums[mid+1]:
-                return nums[mid+1]
-            if nums[mid] < nums[mid-1]:
-                return nums[mid]
-            if nums[mid] > nums[0]:
-                left = mid + 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid -1
             else:
-                right = mid - 1
+                left = mid + 1
+        return -1
