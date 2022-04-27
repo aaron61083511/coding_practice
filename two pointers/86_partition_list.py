49ece44968a03c1654cf35f56ae7859d892f0674
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-04-27 09:41:55 -0500

    Create 86_partition_list.py

---
+++ two pointers/86_partition_list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        l1 = less_part = ListNode()
+        l2 = ge_part = ListNode()
+
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+
+        l2.next = None
+        l1.next = ge_part.next
+        return less_part.next
