7db3a7bc42444d91f04e815fa4b6ed23b343ad69
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-06-06 10:51:43 -0500

    Update insert_sort_array.py

--- algorithm/insert_sort_array.py
+++ algorithm/insert_sort_array.py
@@ -11,6 +11,4 @@ def insert_sort_array(arr):
         print('empty array')
     return arr
 
-# assert insert_sort_array([1, 10, 5, 2, 2,3,8])
-# assert insert_sort_array([])
 print(insert_sort_array([1, 10, 5, 2, 2, 3, 8]))
