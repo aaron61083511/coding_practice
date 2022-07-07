94ddd14c8ecd864a59d51e85abe581e6585dcb1f
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-07-07 10:03:40 -0500

    Update bubble_sort_array.py

--- algorithm/bubble_sort_array.py
+++ algorithm/bubble_sort_array.py
@@ -2,9 +2,13 @@ def bubble_sort_array(arr):
     n = len(arr)
     if n != 0:
         for i in range(n):
+            swapped = False
             for j in range(0, n-i-1):
                 if arr[j] > arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+            if not swapped:
+                break
     else:
         print('empty array')
     return arr
