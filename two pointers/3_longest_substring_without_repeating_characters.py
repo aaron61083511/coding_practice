70ad5cda8ef577072a3e0133498f336955f4af5f
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-07-22 16:16:19 -0500

    Update 3_longest_substring_without_repeating_characters.py

--- two pointers/3_longest_substring_without_repeating_characters.py
+++ two pointers/3_longest_substring_without_repeating_characters.py
@@ -1,14 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        mp = {}
-        i = 0
-        ans = 0
+        # n = len(s)
+        # mp = {}
+        # i = 0
+        # ans = 0
+        #
+        # for j in range(n):
+        #     if s[j] in mp:
+        #         i = max(i, mp[s[j]])
+        #     ans = max(ans, j-i+1)
+        #     mp[s[j]] = j + 1
+        #
+        # return ans
 
-        for j in range(n):
-            if s[j] in mp:
-                i = max(i, mp[s[j]])
-            ans = max(ans, j-i+1)
-            mp[s[j]] = j + 1
-
-        return ans
+        i, j, max_l = 0, 0, 0
+        window = set()
+        while j < len(s):
+            while s[j] in window:
+                window.remove(s[i])
+                i += 1
+            window.add(s[j])
+            max_l = max(max_l, len(window))
+            j += 1
+        return max_l
