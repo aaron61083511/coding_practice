b576ac1b423944bf909a8769b8b7bc7c5f6c4515
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-07-22 13:49:26 -0500

    Update 653_2sum_BST.py

--- binary_tree_dfs/653_2sum_BST.py
+++ binary_tree_dfs/653_2sum_BST.py
@@ -23,3 +23,28 @@ class Solution:
                 stack.append(curr.right)
 
         return False
+
+    # another way:
+    # def findTarget(self, root: TreeNode, k: int) -> bool:
+    #     output = []
+    #     self.inorder(root,output)
+    #     l,r = 0 ,len(output) - 1
+    #     while l < r:
+    #         Sum = output[l] + output[r]
+    #         if Sum == k:
+    #             return True
+    #         else:
+    #             if Sum < k:
+    #                 l += 1
+    #             else:
+    #                 r -= 1
+    #     return False
+    #
+    # def inorder(self,root,output):
+    #     if root == None:
+    #         return
+    #     else:
+    #         self.inorder(root.left,output)
+    #         output.append(root.val)
+    #         self.inorder(root.right,output)
+
