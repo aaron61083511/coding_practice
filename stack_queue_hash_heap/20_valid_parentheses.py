0beea33d7b7e9533652d5b28b4cebe750ea7a89a
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-07-25 16:46:41 -0500

    Update 20_valid_parentheses.py

--- stack_queue_hash_heap/20_valid_parentheses.py
+++ stack_queue_hash_heap/20_valid_parentheses.py
@@ -1,14 +1,13 @@
 class Solution:
-    def threeSumSmaller(self, nums: List[int], target: int) -> int:
-        result = 0
-        nums.sort()
-        for i in range(len(nums) - 2):
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-            s = nums[i] + nums[l] + nums[r]
-            if s < target:
-                result += r - l
-                l += 1
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for i in s:
+            if i not in mapping:
+                stack.append(i)
+            elif stack and stack[-1] == mapping[i]:
+                stack.pop()
             else:
-                r -= 1
-        return result
+                return False
+
+        return len(stack) == 0
