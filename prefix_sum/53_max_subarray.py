06c63a256be5f58614331af6d3cf8b46985d0bca
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-10-13 08:20:03 -0500

    Update 53_max_subarray.py

--- prefix_sum/53_max_subarray.py
+++ prefix_sum/53_max_subarray.py
@@ -5,3 +5,16 @@ class Solution:
             cur_sub = max(i, cur_sub+i)
             max_sub = max(max_sub, cur_sub)
         return max_sub
+
+    def maxSubArray_slidingWindow(self, nums: List[int]) -> int:
+        i, j = 0, 0
+        window_sum = 0
+        max_sum = nums[0]
+        while j < len(nums):
+            window_sum += nums[j]
+            j += 1
+            max_sum = max(max_sum, window_sum)
+            while window_sum < 0:
+                window_sum -= nums[i]
+                i += 1
+        return max_sum
