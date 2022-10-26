b47b76baf446c7c9884b36ad7b881b93bb0a5f46
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-10-26 08:01:14 -0500

    update

--- graph/200_number_of_islands.py
+++ graph/200_number_of_islands.py
@@ -1,56 +1,45 @@
 from collections import deque
 def numIslands_bfs(grid):
-    if grid is None or grid[0] is None:
-        return 0
-    n_row, n_col = len(grid), len(grid[0])
-    n_islands = 0
+    if not grid or not grid[0]:
+            return 0
 
-    for row in range(n_row):
-        for col in range(n_col):
-            if grid[row][col] == '1':
-                n_islands += 1
-                grid[row][col] = '0'
-                queue = deque()
-                queue.append([row, col])
-                while queue:
-                    r, c = queue.popleft()
-                    if r - 1 >= 0 and grid[r - 1][c] == '1':
-                        queue.append([r - 1, c])
-                        grid[r - 1][c] = '0'
-                    if r + 1 < n_row and grid[r + 1][c] == '1':
-                        queue.append([r + 1, c])
-                        grid[r + 1][c] = '0'
-                    if c - 1 >= 0 and grid[r][c - 1] == '1':
-                        queue.append([r, c - 1])
-                        grid[r][c - 1] = '0'
-                    if c + 1 < n_col and grid[r][c + 1] == '1':
-                        queue.append([r, c + 1])
-                        grid[r][c + 1] = '0'
+        n_row, n_col = len(grid), len(grid[0])
+        n_island = 0
 
-    return n_islands
+        for row in range(n_row):
+            for col in range(n_col):
+                if grid[row][col] == '1':
+                    n_island += 1
+                    grid[row][col] = '0'
+                    queue = deque()
+                    queue.append((row, col))
+                    while queue:
+                        r, c = queue.popleft()
+                        for dr, dc in [[0,1],[0,-1],[1,0],[-1,0]]:
+                            r1, c1 = r+dr, c+dc
+                            if 0 <= r1 < n_row and 0 <= c1 < n_col and grid[r1][c1] == '1':
+                                queue.append((r1, c1))
+                                grid[r1][c1] = '0'
+        return n_island
 
 def numIslands_dfs(grid):
-    if grid is None or grid[0] is None:
-        return 0
     n_row, n_col = len(grid), len(grid[0])
-    n_islands = 0
+        n_island = 0
 
-    def dfs(grid, row, col):
-        n_row, n_col = len(grid), len(grid[0])
-        if row < 0 or col < 0 or row >= n_row or col >= n_col or grid[row][col] == '0':
-            return None
-        grid[row][col] = '0'
-        dfs(grid, row - 1, col)
-        dfs(grid, row + 1, col)
-        dfs(grid, row, col - 1)
-        dfs(grid, row, col + 1)
+        def dfs(grid, row, col):
+            if row < 0 or col < 0 or row >= n_row or col >= n_col or grid[row][col] == '0':
+                return 0
+            grid[row][col] = '0'
+            for r, c in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+                r1, c1 = row+r, col+c
+                dfs(grid, r1, c1)
+            return 1
 
-    for row in range(n_row):
-        for col in range(n_col):
-            if grid[row][col] == '1':
-                n_islands += 1
-                dfs(grid, row, col)
-    return n_islands
+        for row in range(n_row):
+            for col in range(n_col):
+                n_island += dfs(grid, row, col)
+
+        return n_island
 
 
 grid = [
