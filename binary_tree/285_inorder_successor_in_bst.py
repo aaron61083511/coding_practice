a2f2531a2dba3dca7f9d505ee0ba902f0b707514
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-11-09 19:50:12 -0600

    Update 285_inorder_successor_in_bst.py

--- binary_tree/285_inorder_successor_in_bst.py
+++ binary_tree/285_inorder_successor_in_bst.py
@@ -15,3 +15,22 @@ class Solution:
                 successor = root
                 root = root.left
         return successor
+
+    def inorderSuccessor_alternaltive(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
+        stack = []
+        def helper(node):
+            while node:
+                stack.append(node)
+                node = node.left
+        cur = root
+        helper(cur)
+        while stack:
+            num = stack.pop()
+            if num.right:
+                helper(num.right)
+            if num.val == p.val:
+                if not stack:
+                    return None
+                return stack.pop()
+            
+        return None
