d286fa1b28ec4d28d1f6804f85a308a59df27136
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-11-21 22:27:17 -0600

    Update 560_subarray_sum_equals_k.py

--- prefix_sum/560_subarray_sum_equals_k.py
+++ prefix_sum/560_subarray_sum_equals_k.py
@@ -45,3 +45,27 @@ class Solution:
        # it's important to capture this edge case in order to return the correct number of subarrays that
        # add up to target.
        # if we are guaranteed that the list nums has no zeros, then we can replace the prefix dict with a prefix set
+    def subarraySum_alternative(self, nums: List[int], k: int) -> int:
+        count = curr_sum = 0
+        h = defaultdict(int)
+        
+        for num in nums:
+        # current prefix sum
+            curr_sum += num
+            
+            # situation 1:
+            # continuous subarray starts 
+            # from the beginning of the array
+        if curr_sum == k:
+            count += 1
+            
+            # situation 2:
+            # number of times the curr_sum − k has occurred already, 
+            # determines the number of times a subarray with sum k 
+            # has occurred up to the current index
+        count += h[curr_sum - k]
+            
+            # add the current sum
+        h[curr_sum] += 1
+                
+        return count
