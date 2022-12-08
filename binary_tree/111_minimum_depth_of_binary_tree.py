aefca304162dc785f6fd07d03191d79e1745d4ce
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2022-12-08 14:19:46 -0600

    Update 111_minimum_depth_of_binary_tree.py

--- binary_tree/111_minimum_depth_of_binary_tree.py
+++ binary_tree/111_minimum_depth_of_binary_tree.py
@@ -19,3 +19,21 @@ class Solution:
                 if node.right:
                     queue.append((l+1, node.right))
         return l
+    
+    def minDepth_II(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+        depth = 0
+        if not root:
+            return depth
+        while queue:
+            depth += 1
+            size = len(queue)
+            for _ in range(size):
+                node = queue.popleft()
+                if not (node.left or node.right):
+                    return depth
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth
