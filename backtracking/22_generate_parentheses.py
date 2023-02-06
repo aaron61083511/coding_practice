9215b38228e19f0694296101f2237c0e5511cc13
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2023-02-06 08:17:21 -0600

    update

--- backtracking/22_generate_parentheses.py
+++ backtracking/22_generate_parentheses.py
@@ -10,3 +10,19 @@ class Solution:
                 helper(s+')', left, right+1)
         helper('', 0, 0)
         return res
+
+        # ans = []
+        # def backtrack(S = [], left = 0, right = 0):
+        #     if len(S) == 2 * n:
+        #         ans.append("".join(S))
+        #         return
+        #     if left < n:
+        #         S.append("(")
+        #         backtrack(S, left+1, right)
+        #         S.pop()
+        #     if right < left:
+        #         S.append(")")
+        #         backtrack(S, left, right+1)
+        #         S.pop()
+        # backtrack()
+        # return ans
