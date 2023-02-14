3f8eb4f22d8e4432164a9c35686f861cd416269c
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2023-02-13 19:19:22 -0600

    Update 700_search_in_bst.py

--- binary_tree/700_search_in_bst.py
+++ binary_tree/700_search_in_bst.py
@@ -5,10 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    def searchBST_I(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         while root is not None and root.val != val:
             if root.val > val:
                 root = root.left
             else:
                 root = root.right
         return root
+
+    def searchBST_R(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root or root.val == val:
+            return root
+        return self.searchBST_R(root.left, val) if root.val > val else self.searchBST_R(root.right, val)
