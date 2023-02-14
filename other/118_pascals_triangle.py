5a98acd7d082186431f404f8973282abce571173
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2023-02-14 14:14:08 -0600

    Update 118_pascals_triangle.py

--- other/118_pascals_triangle.py
+++ other/118_pascals_triangle.py
@@ -1,11 +1,31 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0: return [] # Base Case
-        if numRows == 1: return [[1]] # Base Case
+        if numRows == 0:
+            return
+        if numRows == 1:
+            return [[1]]
         row = []
-        last = self.generate(numRows - 1) # Recursion
+        res = self.generate(numRows-1)
         for i in range(numRows):
-            if i == 0 or i == numRows - 1: row.append(1)
-            else: row.append(last[-1][i-1] + last[-1][i])
-        last.append(row)
-        return last
+            if i == 0 or i == numRows - 1:
+                row.append(1)
+            else:
+                row.append(res[-1][i-1]+res[-1][i])
+        res.append(row)
+        return res
+    
+    def generate_iterative(self, numRows: int) -> List[List[int]]:
+        res = []
+        def helper(row_num):
+            if row_num == 0:
+                return [1]
+            row = []
+            for i in range(row_num+1):
+                if i == 0 or i == row_num:
+                    row.append(1)
+                else:
+                    row.append(res[row_num-1][i-1] + res[row_num-1][i])
+            return row
+        for i in range(numRows):
+            res.append(helper(i))
+        return res
