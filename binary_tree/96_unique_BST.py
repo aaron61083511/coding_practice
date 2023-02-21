0bbe83fb18830c748bca0b3ac656b7bc96668aa7
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2023-02-21 08:34:08 -0600

    Update 96_unique_BST.py

--- binary_tree/96_unique_BST.py
+++ binary_tree/96_unique_BST.py
@@ -14,3 +14,14 @@ class Solution:
             return ans
 
         return dfs(1, n)
+
+    def numTrees_dp(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+        
+        # calculate the number of unique BSTs for i nodes
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        
+        return dp[n]
