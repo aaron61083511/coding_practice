9062d7a153c40c9b410be8b9b7fbd9dc2505b5ac
aaron61083511 <29603368+aaron61083511@users.noreply.github.com>
2023-02-24 08:43:39 -0600

    Update 79_word_search.py

--- backtracking/79_word_search.py
+++ backtracking/79_word_search.py
@@ -36,3 +36,31 @@ class Solution:
                     path = path[:1]
                     board[row][col] = word[0]
         return False
+
+
+        # def dfs(word, i, j, match_idx, board, visited):
+        #     if match_idx == len(word) - 1:
+        #         return True
+
+        #     for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        #         x = i + dx
+        #         y = j + dy
+        #         if 0 <= x < len(board) and 0 <= y < len(board[0]) and visited[x][y] == False and board[x][y] == word[match_idx + 1]:
+        #             visited[x][y] = True
+        #             if dfs(word, x, y, match_idx + 1, board, visited):
+        #                 return True
+        #             visited[x][y] = False
+        
+        # m = len(board)
+        # n = len(board[0])
+        # visited = [[False for _ in range(n)] for _ in range(m)]
+        # # find the starting char
+        # for i in range(m):
+        #     for j in range(n):
+        #         if board[i][j] == word[0]:
+        #             visited[i][j] = True
+        #             match_idx = 0
+        #             if dfs(word, i, j, match_idx, board, visited):
+        #                 return True
+        #             visited[i][j] = False
+        # return False
